eecfa9fc09985df64a236c1d014f88718b139d57
klean2050 <k.avramidis@windowslive.com>
2023-04-14 18:30:55 -0700

    general update

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import *
 
 from datasets import EyePACS, VinDR
-from models import EyePACS_Model, VinDR_Model
+from models import GlobalModel
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-root = "/data/avramidi/large_fundus/"
+root = "/data/avramidi/tiny_vindr/"  # large_fundus or tiny_vindr
 hparams = {
-    "dataset": "EyePACS",
+    "dataset": "VinDR",
     "contrastive": False,
     "lr": 1e-3,
     "batch_size": 64,
     "max_epochs": 100,
     "weight_decay": 1e-6,
+    "distort": False,
 }
 data_transform = Compose(
     [
@@ -32,7 +33,7 @@ logger = TensorBoardLogger(
 )
 
 
-def train_eyepacs(contrastive=False, label_transform=False):
+def get_eyepacs(contrastive, label_transform):
     train_dataset = EyePACS(
         root=root,
         split="train",
@@ -47,38 +48,10 @@ def train_eyepacs(contrastive=False, label_transform=False):
         transform=data_transform,
         label_transform=label_transform,
     )
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=hparams["batch_size"],
-        shuffle=True,
-        drop_last=True,
-        num_workers=4,
-    )
-    valid_loader = DataLoader(
-        valid_dataset,
-        batch_size=hparams["batch_size"],
-        shuffle=False,
-        drop_last=False,
-        num_workers=4,
-    )
-
-    model = EyePACS_Model(hparams).to(DEVICE)
-    trainer = pl.Trainer(
-        logger=logger,
-        max_epochs=hparams["max_epochs"],
-        check_val_every_n_epoch=1,
-        log_every_n_steps=5,
-        sync_batchnorm=True,
-        strategy="ddp_find_unused_parameters_false",
-        accelerator="gpu",
-        devices="auto",
-        precision=16,
-    )
-    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+    return train_dataset, valid_dataset, None
 
 
-def train_vindr(contrastive=False, label_transform=False):
+def get_vindr(contrastive, label_transform):
     train_dataset = VinDR(
         root=root,
         split="train",
@@ -93,6 +66,27 @@ def train_vindr(contrastive=False, label_transform=False):
         transform=data_transform,
         label_transform=label_transform,
     )
+    test_dataset = VinDR(
+        root=root,
+        split="test",
+        contrastive=contrastive,
+        transform=data_transform,
+        label_transform=label_transform,
+    )
+    return train_dataset, valid_dataset, test_dataset
+
+
+if __name__ == "__main__":
+
+    train_dataset, valid_dataset, test_dataset = (
+        get_vindr(
+            contrastive=hparams["contrastive"], label_transform=hparams["distort"]
+        )
+        if hparams["dataset"] == "VinDR"
+        else get_eyepacs(
+            contrastive=hparams["contrastive"], label_transform=hparams["distort"]
+        )
+    )
 
     train_loader = DataLoader(
         train_dataset,
@@ -108,22 +102,30 @@ def train_vindr(contrastive=False, label_transform=False):
         drop_last=False,
         num_workers=4,
     )
+    test_loader = (
+        DataLoader(
+            test_dataset,
+            batch_size=hparams["batch_size"],
+            shuffle=False,
+            drop_last=False,
+            num_workers=4,
+        )
+        if test_dataset is not None
+        else None
+    )
 
-    model = VinDR_Model(hparams).to(DEVICE)
+    model = GlobalModel(hparams).to(device)
     trainer = pl.Trainer(
+        logger=logger,
         max_epochs=hparams["max_epochs"],
-        devices="auto",
+        check_val_every_n_epoch=1,
+        log_every_n_steps=5,
+        sync_batchnorm=True,
+        strategy="ddp_find_unused_parameters_false",
         accelerator="gpu",
+        devices="auto",
         precision=16,
     )
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
-
-
-if __name__ == "__main__":
-
-    if hparams["dataset"] == "EyePACS":
-        print("Training for EyePACS...")
-        train_eyepacs(contrastive=hparams["contrastive"], label_transform=False)
-    elif hparams["dataset"] == "VinDR":
-        print("Training for VinDR...")
-        train_vindr(contrastive=False, label_transform=True)
+    if test_loader is not None:
+        trainer.test(model, test_dataloaders=test_loader)
